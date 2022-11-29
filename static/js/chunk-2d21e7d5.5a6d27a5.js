cc9ee2a7e230e3d9516dc21c064085c6ab69db09
chengdengnan <572298826@qq.com>
2022-11-29 23:12:38 +0800

    fix home page background image and perfect JavaScript basic.

---
+++ static/js/chunk-2d21e7d5.5a6d27a5.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21e7d5"],{d67d:function(n,d,e){"use strict";function u(n,d){return null}e.r(d);var c=e("6b0d"),o=e.n(c);const r={},t=o()(r,[["render",u]]);d["default"]=t}}]);
